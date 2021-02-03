8c9f17ddd33cfbbf3c4ce5c66d019223b3d87bb7
Antonio Tobajas <atobajas@gmail.com>
2021-02-03 20:37:24 +0100

    Changed in comments and name of container Jenkins

--- 03-CiCd/start_jenkins.sh
+++ 03-CiCd/start_jenkins.sh
@@ -5,8 +5,11 @@ CERTS_VOLUME=$3
 DATA_VOLUME=$4
 # Reference: https://forums.docker.com/t/how-to-filter-docker-ps-by-exact-name/2880/3
 # Ensure that script have permissions to be executed: chmod +x start_jenkins.sh
-# Build image from Dockerfile: docker build -t jenkins-gradle .
+# Build image from Dockerfile: 
+#     docker build -t jenkins-docker-gradle -f DockerfilewithDockerClient .
+#     docker build -t jenkins-gradle .
 # From previous image we can run custom Jenkins version as follows:
+# ./start_jenkins.sh jenkins-docker-gradle jenkins jenkins-docker-certs-gradle jenkins-data-gradle
 # ./start_jenkins.sh jenkins-gradle jenkins jenkins-docker-certs-gradle jenkins-data-gradle
 if [[ ! "$(docker network ls --format '{{.Name}}' --filter name=^$NETWORK$)" ]]; then
   docker network create $NETWORK
@@ -30,7 +33,7 @@ docker container run --name jenkins-docker --rm --detach \
     --volume "$DATA_VOLUME":/var/jenkins_home \
     --publish 2376:2376 docker:dind
 # Start Jenkins in the same network
-docker container run --name jenkins-docker-gradle --rm --detach \
+docker container run --name "$IMAGE" --rm --detach \
   --network jenkins --env DOCKER_HOST=tcp://docker:2376 \
   --env DOCKER_CERT_PATH=/certs/client --env DOCKER_TLS_VERIFY=1 \
   --volume "$CERTS_VOLUME":/certs/client:ro \
